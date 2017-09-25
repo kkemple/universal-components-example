ea236f6bf75cf208ac97e74c7592ec18782414eb
Kurtis Kemple <kurtiskemple@gmail.com>
2017-09-25 15:24:37 -0400

    docs(project): add project docs

--- examples/web/src/App.js
+++ examples/web/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import { Button } from '@kkemple/universal-components';
 
 class App extends Component {
