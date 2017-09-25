4ee9c56d48be08c3308dbd0a19d9c1d615ca09a7
Kurtis Kemple <kurtiskemple@gmail.com>
2017-09-25 10:11:52 -0400

    Web example (#2)
    * feat(web): add web example
    
    * feat(web example): add base web example
    
    * fix(web example): rebuild app
    
    * feat(web example): add universal components

---
+++ examples/web/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { Button } from '@kkemple/universal-components';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+          <div className="Button-wrapper">
+            <Button
+              text="Press on Web!"
+              onPress={() => alert('Button Pressed!')}
+            />
+          </div>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
